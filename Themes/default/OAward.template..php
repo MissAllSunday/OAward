8e08d92a3048688db356d041493f48d25f852a3a
Suki <suki@missallsunday.com>
2013-07-04 10:15:03 -0500

    Couple more files
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ Themes/default/OAward.template..php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *
+ * @package awards mod
+ * @version 1.0
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2013, Jessica González
+ * @license http://www.mozilla.org/MPL/MPL-1.1.html
+ */
