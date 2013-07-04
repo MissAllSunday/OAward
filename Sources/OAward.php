7492a058590d1038261198e13c1c5b7f7f1dba6c
Suki <suki@missallsunday.com>
2013-07-04 11:00:12 -0500

    Some logic
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/OAward.php
+++ Sources/OAward.php
@@ -13,31 +13,62 @@ if (!defined('SMF'))
 
 class OAward
 {
+	// Set a nice name to avoid having to write the same thing over anf over again...
 	public static $name = 'OAward';
+
+	// Hard-coded CRUD actions FTW!
+	protected static $actions = array('create', 'read', 'update', 'delete');
+	protected $error = false;
+
+	public function ajax()
+	{
+		// Yeah, we're using superglobals directly, ugly but when in Rome, do as the Romans do...
+		$sa = trim(htmlspecialchars($_GET['sa'], ENT_QUOTES));
+
+		// Nothing to see here, move on...
+		if (empty($sa) or !in_array($sa, self::$actions))
+			fatal_lang_error(self::$name .'_error_no_valid_action', false);
+
+		// Time to instantiate yourself pal...
+		$do = new self();
+
+		// Leave to each case to solve things on their own...
+		$do->$sa();
+
+		// We got an issue...
+		if (!empty($this->error))
+			fatal_lang_error(self::$name .'_error_'. $this->error, false);
+
+		// Everything went better than expected
+		else
+			// od stuff here!
+	}
 
 	public function __construct()
 	{
+		loadLanguage(self::$name);
+
+		$this->_data = $_REQUEST;
 
 	}
 
-	public function create($data)
+	public function create()
 	{
 
 	}
 
-	public function read($data)
+	public function read()
 	{
 
 	}
 
-	public function edit($data)
+	public function update()
 	{
 
 	}
 
-	public function delete($data)
+	public function delete()
 	{
 
 	}
-
 }
