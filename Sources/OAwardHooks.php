e91dfc22a29dd568383b1b2002e020a1eac87877
Suki <suki@missallsunday.com>
2013-10-21 18:09:40 -0500

    Don't use Sources => $boarddir
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/OAwardHooks.php
+++ Sources/OAwardHooks.php
@@ -3,8 +3,8 @@
  *
  * @package OAwards mod
  * @version 1.0
- * @author Jessica Gonz√°lez <suki@missallsunday.com>
- * @copyright Copyright (c) 2013, Jessica Gonz√°lez
+ * @author Jessica Gonz·lez <suki@missallsunday.com>
+ * @copyright Copyright (c) 2013, Jessica Gonz·lez
  * @license http://www.mozilla.org/MPL/ MPL 2.0
  */
 
